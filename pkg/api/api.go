6eed4cb7e534ab8717215cf824173242e948622d
Quentin Perez <qperez@ocs.online.net>
2016-02-02 14:37:15 +0100

    Add delete

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -20,13 +20,13 @@ func New(host, accessKey, secretKey string) *API {
 	return &API{host, accessKey, secretKey}
 }
 
-func (api *API) get(route string, args url.Values) (body []byte, err error) {
+func (api *API) get(route string, args url.Values) (body []byte, statusCode int, err error) {
 	client := http.Client{}
 	url := fmt.Sprintf("%v%v?%s", api.host, route, args.Encode())
 	fmt.Println("URL:", url)
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		return nil, err
+		return nil, 0, err
 	}
 	awsauth.SignS3(req, awsauth.Credentials{
 		AccessKeyID:     api.accessKey,
@@ -38,7 +38,34 @@ func (api *API) get(route string, args url.Values) (body []byte, err error) {
 		defer resp.Body.Close()
 	}
 	if err != nil {
-		return nil, err
+		return nil, 0, err
 	}
-	return ioutil.ReadAll(resp.Body)
+	body, err = ioutil.ReadAll(resp.Body)
+	statusCode = resp.StatusCode
+	return
+}
+
+func (api *API) delete(route string, args url.Values) (body []byte, statusCode int, err error) {
+	client := http.Client{}
+	url := fmt.Sprintf("%v%v?%s", api.host, route, args.Encode())
+	fmt.Println("URL:", url)
+	req, err := http.NewRequest("DELETE", url, nil)
+	if err != nil {
+		return nil, 0, err
+	}
+	awsauth.SignS3(req, awsauth.Credentials{
+		AccessKeyID:     api.accessKey,
+		SecretAccessKey: api.secretKey,
+		Expiration:      time.Now().Add(1 * time.Minute)},
+	)
+	resp, err := client.Do(req)
+	if resp != nil {
+		defer resp.Body.Close()
+	}
+	if err != nil {
+		return nil, 0, err
+	}
+	body, err = ioutil.ReadAll(resp.Body)
+	statusCode = resp.StatusCode
+	return
 }
