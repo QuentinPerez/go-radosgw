39bfe240ebaac38d21ac51625502e8fc715c2124
Quentin Perez <qperez@ocs.online.net>
2016-02-02 14:37:29 +0100

    Add DeleteUsage

--- pkg/api/helpers.go
+++ pkg/api/helpers.go
@@ -13,6 +13,7 @@ type UsageConfig struct {
 	End         *time.Time
 	ShowEntries bool
 	ShowSummary bool
+	RemoveAll   bool
 }
 
 func (api *API) GetUsage(conf *UsageConfig) (*Usage, error) {
@@ -43,7 +44,7 @@ func (api *API) GetUsage(conf *UsageConfig) (*Usage, error) {
 			values.Add("end", timeStamp)
 		}
 	}
-	body, err := api.get("/admin/usage", values)
+	body, _, err := api.get("/admin/usage", values)
 	if err != nil {
 		return nil, err
 	}
@@ -51,4 +52,35 @@ func (api *API) GetUsage(conf *UsageConfig) (*Usage, error) {
 		return nil, err
 	}
 	return ret, nil
+}
+
+func (api *API) DeleteUsage(conf *UsageConfig) error {
+	values := url.Values{}
+
+	values.Add("format", "json")
+	if conf != nil {
+		if conf.UID != "" {
+			values.Add("uid", conf.UID)
+		}
+		if conf.RemoveAll {
+			values.Add("remove-all", "True")
+		}
+		if conf.Start != nil {
+			timeStamp := fmt.Sprintf("%v-%d-%v %v:%v:%v",
+				conf.Start.Year(), conf.Start.Month(), conf.Start.Day(),
+				conf.Start.Hour(), conf.Start.Minute(), conf.Start.Second())
+			values.Add("start", timeStamp)
+		}
+		if conf.End != nil {
+			timeStamp := fmt.Sprintf("%v-%d-%v %v:%v:%v",
+				conf.End.Year(), conf.End.Month(), conf.End.Day(),
+				conf.End.Hour(), conf.End.Minute(), conf.End.Second())
+			values.Add("end", timeStamp)
+		}
+	}
+	_, _, err := api.delete("/admin/usage", values)
+	if err != nil {
+		return err
+	}
+	return nil
 }
