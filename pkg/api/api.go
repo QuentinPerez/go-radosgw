09b40667ae09be390d61d4623965171be4df877a
Quentin Perez <qperez@ocs.online.net>
2016-02-05 13:17:55 +0100

    Add makerequest

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -1,6 +1,8 @@
 package radosAPI
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -21,13 +23,14 @@ func New(host, accessKey, secretKey string) *API {
 	return &API{host, accessKey, secretKey}
 }
 
-func (api *API) get(route string, args url.Values) (body []byte, statusCode int, err error) {
+func (api *API) makeRequest(verb, url string) (body []byte, statusCode int, err error) {
+	var apiErr apiError
 	client := http.Client{}
-	url := fmt.Sprintf("%v%v?%s", api.host, route, args.Encode())
-	fmt.Println("URL:", url)
-	req, err := http.NewRequest("GET", url, nil)
+
+	// fmt.Println("URL:", url)
+	req, err := http.NewRequest(verb, url, nil)
 	if err != nil {
-		return nil, 0, err
+		return
 	}
 	awsauth.SignS3(req, awsauth.Credentials{
 		AccessKeyID:     api.accessKey,
@@ -38,35 +41,24 @@ func (api *API) get(route string, args url.Values) (body []byte, statusCode int,
 	if resp != nil {
 		defer resp.Body.Close()
 	}
+	statusCode = resp.StatusCode
 	if err != nil {
-		return nil, 0, err
+		return
 	}
 	body, err = ioutil.ReadAll(resp.Body)
-	statusCode = resp.StatusCode
+	if err != nil {
+		return
+	}
+	if err = json.Unmarshal(body, &apiErr); err == nil && apiErr.Code != "" {
+		err = errors.New(apiErr.Code)
+	}
 	return
 }
 
+func (api *API) get(route string, args url.Values) (body []byte, statusCode int, err error) {
+	return api.makeRequest("GET", fmt.Sprintf("%v%v?%s", api.host, route, args.Encode()))
+}
+
 func (api *API) delete(route string, args url.Values) (body []byte, statusCode int, err error) {
-	client := http.Client{}
-	url := fmt.Sprintf("%v%v?%s", api.host, route, args.Encode())
-	fmt.Println("URL:", url)
-	req, err := http.NewRequest("DELETE", url, nil)
-	if err != nil {
-		return nil, 0, err
-	}
-	awsauth.SignS3(req, awsauth.Credentials{
-		AccessKeyID:     api.accessKey,
-		SecretAccessKey: api.secretKey,
-		Expiration:      time.Now().Add(1 * time.Minute)},
-	)
-	resp, err := client.Do(req)
-	if resp != nil {
-		defer resp.Body.Close()
-	}
-	if err != nil {
-		return nil, 0, err
-	}
-	body, err = ioutil.ReadAll(resp.Body)
-	statusCode = resp.StatusCode
-	return
+	return api.makeRequest("DELETE", fmt.Sprintf("%v%v?%s", api.host, route, args.Encode()))
 }
