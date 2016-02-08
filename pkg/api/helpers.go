37dd44cab1dcb7391a45896a1a96b41f23321acb
Quentin Perez <qperez@ocs.online.net>
2016-02-08 11:12:23 +0100

    Use EncodeUrl

--- pkg/api/helpers.go
+++ pkg/api/helpers.go
@@ -2,36 +2,20 @@ package radosAPI
 
 import (
 	"encoding/json"
-	"errors"
-	"fmt"
 	"net/url"
 	"time"
 
-	"github.com/QuentinPerez/go-radosgw/pkg/structToUrl"
+	"github.com/QuentinPerez/go-encodeUrl"
 )
-
-func init() {
-	structToUrl.Funcs["ifNotEmpty"] = ifNotEmpty
-}
 
 // UsageConfig usage request
 type UsageConfig struct {
-	UID         string     `url:"uid,ifNotEmpty"` // The user for which the information is requested. If not specified will apply to all users
-	Start       *time.Time `url:"start"`          // Date and (optional) time that specifies the start time of the requested data
-	End         *time.Time `url:"end"`            // Date and (optional) time that specifies the end time of the requested data (non-inclusive)
-	ShowEntries bool       `url:"show-entries"`   // Specifies whether data entries should be returned.
-	ShowSummary bool       `url:"show-summary"`   // Specifies whether data summary should be returned
-	RemoveAll   bool       `url:"remove-all"`     // Required when uid is not specified, in order to acknowledge multi user data removal.
-}
-
-func ifNotEmpty(obj interface{}) (string, bool, error) {
-	if val, ok := obj.(string); ok {
-		if val != "" {
-			return val, true, nil
-		}
-		return "", false, nil
-	}
-	return "", false, errors.New("this field should be a string")
+	UID         string     `url:"uid,ifStringIsNotEmpty"`     // The user for which the information is requested. If not specified will apply to all users
+	Start       *time.Time `url:"start,ifTimeIsNotNilCeph"`   // Date and (optional) time that specifies the start time of the requested data
+	End         *time.Time `url:"end,ifTimeIsNotNilCeph"`     // Date and (optional) time that specifies the end time of the requested data (non-inclusive)
+	ShowEntries bool       `url:"show-entries,ifBoolIsFalse"` // Specifies whether data entries should be returned.
+	ShowSummary bool       `url:"show-summary,ifBoolIsFalse"` // Specifies whether data summary should be returned
+	RemoveAll   bool       `url:"remove-all,ifBoolIsTrue"`    // Required when uid is not specified, in order to acknowledge multi user data removal.
 }
 
 // GetUsage requests bandwidth usage information.
@@ -45,29 +29,17 @@ func ifNotEmpty(obj interface{}) (string, bool, error) {
 // @ShowSummary
 //
 func (api *API) GetUsage(conf *UsageConfig) (*Usage, error) {
-	ret := &Usage{}
-
-	values := structToUrl.Translate(conf)
+	var (
+		ret    = &Usage{}
+		values = url.Values{}
+		errs   []error
+	)
 
 	values.Add("format", "json")
 	if conf != nil {
-		if !conf.ShowEntries {
-			values.Add("show-entries", "False")
-		}
-		if !conf.ShowSummary {
-			values.Add("show-summary", "False")
-		}
-		if conf.Start != nil {
-			timeStamp := fmt.Sprintf("%v-%d-%v %v:%v:%v",
-				conf.Start.Year(), conf.Start.Month(), conf.Start.Day(),
-				conf.Start.Hour(), conf.Start.Minute(), conf.Start.Second())
-			values.Add("start", timeStamp)
-		}
-		if conf.End != nil {
-			timeStamp := fmt.Sprintf("%v-%d-%v %v:%v:%v",
-				conf.End.Year(), conf.End.Month(), conf.End.Day(),
-				conf.End.Hour(), conf.End.Minute(), conf.End.Second())
-			values.Add("end", timeStamp)
+		values, errs = encurl.Translate(conf)
+		if len(errs) > 0 {
+			return nil, errs[0]
 		}
 	}
 	body, _, err := api.get("/admin/usage", values)
@@ -90,27 +62,16 @@ func (api *API) GetUsage(conf *UsageConfig) (*Usage, error) {
 // @RemoveAll
 //
 func (api *API) DeleteUsage(conf *UsageConfig) error {
-	values := url.Values{}
+	var (
+		values = url.Values{}
+		errs   []error
+	)
 
 	values.Add("format", "json")
 	if conf != nil {
-		if conf.UID != "" {
-			values.Add("uid", conf.UID)
-		}
-		if conf.RemoveAll {
-			values.Add("remove-all", "True")
-		}
-		if conf.Start != nil {
-			timeStamp := fmt.Sprintf("%v-%d-%v %v:%v:%v",
-				conf.Start.Year(), conf.Start.Month(), conf.Start.Day(),
-				conf.Start.Hour(), conf.Start.Minute(), conf.Start.Second())
-			values.Add("start", timeStamp)
-		}
-		if conf.End != nil {
-			timeStamp := fmt.Sprintf("%v-%d-%v %v:%v:%v",
-				conf.End.Year(), conf.End.Month(), conf.End.Day(),
-				conf.End.Hour(), conf.End.Minute(), conf.End.Second())
-			values.Add("end", timeStamp)
+		values, errs = encurl.Translate(conf)
+		if len(errs) > 0 {
+			return errs[0]
 		}
 	}
 	_, _, err := api.delete("/admin/usage", values)
