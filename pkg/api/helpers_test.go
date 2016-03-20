5db8b2d318df4addd561b98b2023367bbcc046b6
Quentin Perez <qperez@ocs.online.net>
2016-03-20 13:14:54 +0100

    test: Add user unittest

--- pkg/api/helpers_test.go
+++ pkg/api/helpers_test.go
@@ -22,3 +22,56 @@ func TestNewAPIWithPrefix(t *testing.T) {
 		So(api, ShouldNotBeNil)
 	})
 }
+
+func TestCreateUser(t *testing.T) {
+	Convey("Testing Create user", t, func() {
+		api := New(os.Getenv("RADOSGW_API"), os.Getenv("RADOSGW_ACCESS"), os.Getenv("RADOSGW_SECRET"), "admin")
+
+		user, err := api.CreateUser(UserConfig{
+			UID:         "UnitTest",
+			DisplayName: "Unit Test",
+		})
+		So(err, ShouldBeNil)
+		So(user, ShouldNotBeNil)
+		So(user.DisplayName, ShouldEqual, "Unit Test")
+	})
+}
+
+func TestGetUser(t *testing.T) {
+	Convey("Testing Get user", t, func() {
+		api := New(os.Getenv("RADOSGW_API"), os.Getenv("RADOSGW_ACCESS"), os.Getenv("RADOSGW_SECRET"))
+
+		user, err := api.GetUser("UnitTest")
+		So(err, ShouldBeNil)
+		So(user, ShouldNotBeNil)
+	})
+}
+
+func TestUpdateUser(t *testing.T) {
+	Convey("Testing Update user", t, func() {
+		api := New(os.Getenv("RADOSGW_API"), os.Getenv("RADOSGW_ACCESS"), os.Getenv("RADOSGW_SECRET"))
+
+		user, err := api.UpdateUser(UserConfig{
+			UID:   "UnitTest",
+			Email: "UnitTest@test.com",
+		})
+		So(err, ShouldBeNil)
+		So(user, ShouldNotBeNil)
+		So(user.Email, ShouldEqual, "UnitTest@test.com")
+	})
+}
+
+func TestRemoveUser(t *testing.T) {
+	Convey("Testing Remove user", t, func() {
+		api := New(os.Getenv("RADOSGW_API"), os.Getenv("RADOSGW_ACCESS"), os.Getenv("RADOSGW_SECRET"))
+
+		err := api.RemoveUser(UserConfig{
+			UID:       "UnitTest",
+			PurgeData: true,
+		})
+		So(err, ShouldBeNil)
+		user, err := api.GetUser("UnitTest")
+		So(err, ShouldNotBeNil)
+		So(user, ShouldBeNil)
+	})
+}
