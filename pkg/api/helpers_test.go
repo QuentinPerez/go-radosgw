52df8e47faf43d7e74caf35ae40821d240bbffbc
Quentin Perez <qperez@ocs.online.net>
2016-03-28 17:17:43 +0200

    test: add usage

--- pkg/api/helpers_test.go
+++ pkg/api/helpers_test.go
@@ -3,6 +3,7 @@ package radosAPI
 import (
 	"os"
 	"testing"
+	"time"
 
 	. "github.com/smartystreets/goconvey/convey"
 )
@@ -109,5 +110,117 @@ func TestRemoveUser(t *testing.T) {
 		user, err := api.GetUser("UnitTest")
 		So(err, ShouldNotBeNil)
 		So(user, ShouldBeNil)
+	})
+}
+
+func TestRemoveUserWithoutUID(t *testing.T) {
+	Convey("Testing Remove user without UID", t, func() {
+		api := New(os.Getenv("RADOSGW_API"), os.Getenv("RADOSGW_ACCESS"), os.Getenv("RADOSGW_SECRET"))
+
+		err := api.RemoveUser(UserConfig{
+			PurgeData: true,
+		})
+		So(err, ShouldNotBeNil)
+	})
+}
+
+func TestGetUsageEmpty(t *testing.T) {
+	Convey("Testing Get Usage with empty struct", t, func() {
+		api := New(os.Getenv("RADOSGW_API"), os.Getenv("RADOSGW_ACCESS"), os.Getenv("RADOSGW_SECRET"))
+
+		usage, err := api.GetUsage(UsageConfig{})
+		So(err, ShouldBeNil)
+		So(usage, ShouldNotBeNil)
+		So(usage.Entries, ShouldBeNil)
+		So(usage.Summary, ShouldBeNil)
+	})
+}
+
+func TestGetUsageSummary(t *testing.T) {
+	Convey("Testing Get Usage summary field", t, func() {
+		api := New(os.Getenv("RADOSGW_API"), os.Getenv("RADOSGW_ACCESS"), os.Getenv("RADOSGW_SECRET"))
+
+		usage, err := api.GetUsage(UsageConfig{
+			ShowSummary: true,
+		})
+		So(err, ShouldBeNil)
+		So(usage, ShouldNotBeNil)
+		So(usage.Entries, ShouldBeNil)
+		So(usage.Summary, ShouldNotBeNil)
+	})
+}
+
+func TestGetUsageEntries(t *testing.T) {
+	Convey("Testing Get Usage entries field", t, func() {
+		api := New(os.Getenv("RADOSGW_API"), os.Getenv("RADOSGW_ACCESS"), os.Getenv("RADOSGW_SECRET"))
+
+		usage, err := api.GetUsage(UsageConfig{
+			ShowEntries: true,
+		})
+		So(err, ShouldBeNil)
+		So(usage, ShouldNotBeNil)
+		So(usage.Entries, ShouldNotBeNil)
+		So(usage.Summary, ShouldBeNil)
+	})
+}
+
+func TestGetUsageEntriesSummary(t *testing.T) {
+	Convey("Testing Get Usage entries/summary field", t, func() {
+		api := New(os.Getenv("RADOSGW_API"), os.Getenv("RADOSGW_ACCESS"), os.Getenv("RADOSGW_SECRET"))
+
+		usage, err := api.GetUsage(UsageConfig{
+			ShowEntries: true,
+			ShowSummary: true,
+		})
+		So(err, ShouldBeNil)
+		So(usage, ShouldNotBeNil)
+		So(usage.Entries, ShouldNotBeNil)
+		So(usage.Summary, ShouldNotBeNil)
+	})
+}
+
+func TestGetUsageEntriesSummaryWithUID(t *testing.T) {
+	Convey("Testing Get Usage entries/summary field with specified uid", t, func() {
+		api := New(os.Getenv("RADOSGW_API"), os.Getenv("RADOSGW_ACCESS"), os.Getenv("RADOSGW_SECRET"))
+
+		usage, err := api.GetUsage(UsageConfig{
+			UID:         "UnitTest",
+			ShowEntries: true,
+			ShowSummary: true,
+		})
+		So(err, ShouldBeNil)
+		So(usage, ShouldNotBeNil)
+		So(usage.Entries, ShouldNotBeNil)
+		So(usage.Summary, ShouldNotBeNil)
+	})
+}
+
+func TestGetUsageEntriesSummaryWithUIDStartTime(t *testing.T) {
+	Convey("Testing Get Usage entries/summary field with specified uid, and start Time", t, func() {
+		api := New(os.Getenv("RADOSGW_API"), os.Getenv("RADOSGW_ACCESS"), os.Getenv("RADOSGW_SECRET"))
+
+		now := time.Now().AddDate(0, 0, -1)
+		usage, err := api.GetUsage(UsageConfig{
+			UID:         "UnitTest",
+			ShowEntries: true,
+			ShowSummary: true,
+			Start:       &now,
+		})
+		So(err, ShouldBeNil)
+		So(usage, ShouldNotBeNil)
+		So(usage.Entries, ShouldNotBeNil)
+		So(usage.Summary, ShouldNotBeNil)
+	})
+}
+
+func TestDeleteAllUsages(t *testing.T) {
+	Convey("Testing Delete all usages", t, func() {
+		api := New(os.Getenv("RADOSGW_API"), os.Getenv("RADOSGW_ACCESS"), os.Getenv("RADOSGW_SECRET"))
+
+		err := api.DeleteUsage(UsageConfig{
+			UID:       "UnitTest",
+			RemoveAll: true,
+		})
+		So(err, ShouldBeNil)
 	})
 }
